92f02f16c19c01a909623ed5035561a83e54057d
Distraw <dmitroisuk11@gmail.com>
2026-01-15 12:45:46 +0200

    feat: add tables to db to store tracked bitcoin addresses (addresses, users_addresses)

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -8,6 +8,17 @@ CREATE TABLE users (
     password BYTEA NOT NULL
 );
 
+CREATE TABLE addresses (
+    id SERIAL PRIMARY KEY,
+    addr TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE users_addresses (
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    address_id INT REFERENCES addresses(id) ON DELETE CASCADE,
+    PRIMARY KEY(user_id, address_id)
+);
+
 -- +migrate Down
 
 DROP USER heathchecker;
