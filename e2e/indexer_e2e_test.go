4792221c91a637c548370431946d90b51c29f873
Distraw <dmitroisuk11@gmail.com>
2026-02-09 16:14:47 +0200

    tests: add catching-up and reorganizing tests

--- e2e/indexer_e2e_test.go
+++ e2e/indexer_e2e_test.go
@@ -9,19 +9,28 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestIndexer(t *testing.T) {
-	fmt.Println("Building docker image...")
-	buildImage(t)
+var (
+	imageBuilt = false
+)
+
+func TestIndexer_Syncing(t *testing.T) {
+	if !imageBuilt {
+		fmt.Println("Building docker image...")
+		buildImage(t)
+		imageBuilt = true
+	}
 
 	fmt.Println("Setupping compose...")
 	setupCompose(t)
 	defer run(t, "docker", "compose", "down", "-v")
 
 	fmt.Println("Setupping regtest node...")
-	addr := setupRegtestNode(t)
+	setupRegtestNode(t)
 
-	fmt.Println("Generating blocks...")
-	generateToAddress(t, addr, 101)
+	addr := getNewAddress(t)
+
+	fmt.Println("Generating genesis block...")
+	generateToAddress(t, addr, 1)
 
 	require.True(t, healthcheck(t).Alive)
 
@@ -35,5 +44,89 @@ func TestIndexer(t *testing.T) {
 		return healthcheck(t).CatchedUp
 	})
 
+	const mineBlocks int = 10
+	for i := 0; i < mineBlocks; i++ {
+		generateToAddress(t, addr, 1)
+		time.Sleep(time.Second)
+	}
+
+	assert.Equal(t, 550, balance(t, token, addr))
+}
+
+func TestIndexer_CatchUp(t *testing.T) {
+	if !imageBuilt {
+		fmt.Println("Building docker image...")
+		buildImage(t)
+		imageBuilt = true
+	}
+
+	fmt.Println("Setupping compose...")
+	setupCompose(t)
+	defer run(t, "docker", "compose", "down", "-v")
+
+	fmt.Println("Setupping regtest node...")
+	setupRegtestNode(t)
+
+	addr := getNewAddress(t)
+
+	fmt.Println("Generating blocks...")
+	generateToAddress(t, addr, 101)
+
+	require.True(t, healthcheck(t).Alive)
+
+	fmt.Println("Authorizing into indexer...")
+	register(t, "user", "123")
+	token := login(t, "user", "123")
+
+	postAddress(t, token, addr)
+	launch(t)
+	waitFor(t, time.Second*10, func() bool {
+		return healthcheck(t).CatchedUp
+	})
+
 	assert.Equal(t, 5050, balance(t, token, addr))
+}
+
+func TestIndexer_Reorganizing(t *testing.T) {
+	if !imageBuilt {
+		fmt.Println("Building docker image...")
+		buildImage(t)
+		imageBuilt = true
+	}
+
+	fmt.Println("Setupping compose...")
+	setupCompose(t)
+	defer run(t, "docker", "compose", "down", "-v")
+
+	fmt.Println("Setupping regtest node...")
+	setupRegtestNode(t)
+
+	addr := getNewAddress(t)
+
+	fmt.Println("Generating blocks...")
+	hashes := generateToAddress(t, addr, 10)
+
+	require.True(t, healthcheck(t).Alive)
+
+	fmt.Println("Authorizing into indexer...")
+	register(t, "user", "123")
+	token := login(t, "user", "123")
+
+	postAddress(t, token, addr)
+	launch(t)
+	waitFor(t, time.Second*10, func() bool {
+		return healthcheck(t).CatchedUp
+	})
+
+	assert.Equal(t, 500, balance(t, token, addr))
+
+	fmt.Println("Invalidating 5th block...")
+	invalidateBlock(t, hashes[len(hashes)-5])
+
+	time.Sleep(time.Second * 15)
+	assert.Equal(t, 250, balance(t, token, addr))
+
+	generateToAddress(t, addr, 3)
+	time.Sleep(time.Second * 15)
+	assert.Equal(t, 400, balance(t, token, addr))
 }
--- e2e/node.go
+++ e2e/node.go
@@ -1,8 +1,11 @@
 package e2e
 
 import (
+	"encoding/json"
 	"strconv"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 const (
@@ -19,13 +22,28 @@ var (
 	}
 )
 
-func setupRegtestNode(t *testing.T) (addr string) {
+func setupRegtestNode(t *testing.T) {
 	t.Helper()
 	run(t, "bitcoin-cli", append(standardArgs, "createwallet", "wallet_0")...)
+}
+
+func getNewAddress(t *testing.T) (addr string) {
+	t.Helper()
 	return run(t, "bitcoin-cli", append(standardArgs, "getnewaddress")...)
 }
 
-func generateToAddress(t *testing.T, addr string, blocks int) {
+func invalidateBlock(t *testing.T, blockhash string) {
 	t.Helper()
-	run(t, "bitcoin-cli", append(standardArgs, "generatetoaddress", strconv.Itoa(blocks), addr)...)
+	run(t, "bitcoin-cli", append(standardArgs, "invalidateblock", blockhash)...)
+}
+
+func generateToAddress(t *testing.T, addr string, blocks int) []string {
+	t.Helper()
+	output := run(t, "bitcoin-cli", append(standardArgs, "generatetoaddress", strconv.Itoa(blocks), addr)...)
+
+	var blocksHashes []string
+
+	err := json.Unmarshal([]byte(output), &blocksHashes)
+	require.NoError(t, err, "failed to unmarshal output from generatetoaddress")
+	return blocksHashes
 }
